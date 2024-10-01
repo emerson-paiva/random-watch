251fd8c4b9c56bce11d2b0037d92fb7682e6d5fd
Emerson Paiva <emerson.paiva@vtex.com>
2024-10-01 09:27:40 -0300

    fix: disabled when empty

--- src/components/Recommend/Recommend.tsx
+++ src/components/Recommend/Recommend.tsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button'
 
 export const Recommend = () => {
   const [movieToWatch, setMovieToWatch] = useState<string>()
-  const { getRandomMovieToWatch } = useWatchListContext()
+  const { watchList, getRandomMovieToWatch } = useWatchListContext()
 
   const handleRecommendation = () => {
     const movie = getRandomMovieToWatch()
@@ -15,7 +15,11 @@ export const Recommend = () => {
   return (
     <div className="mb-8 items-center justify-center flex flex-col">
       <h1 className="mb-8 text-5xl font-bold">{movieToWatch}</h1>
-      <Button size="lg" onClick={handleRecommendation}>
+      <Button
+        size="lg"
+        onClick={handleRecommendation}
+        disabled={watchList.length === 0}
+      >
         {movieToWatch ? 'Não gostei, de novo!' : 'Quero uma recomendação ✨'}
       </Button>
     </div>
